1737f43d5f7132854a6776ec824e8fba774bcc9c
Andrea Colleoni <andrea.colleoni@outlook.it>
2022-06-09 17:13:58 +0200

    esercizio

---
+++ esercizio_04.py
@@ -0,0 +1,10 @@
+# Creare una webapp con Flask chiamata primaApp
+
+# Definire tutto quanto serve all'app nel package python "app", come fatto a lezione
+# (__init__, routes, ecc.)
+
+# 1. Definire una rotta "/casuali" che visualizzi 10 numeri casuali in una pagina web
+# 2. Definire una rotta "/tabellina-7" che visualizzi le prima 50 voci della tabellina del 7
+
+# Per visualizzare gli elenchi si possono usare <table>, <ul>, <ol>, oppure dei normali paragrafi <p>
+
