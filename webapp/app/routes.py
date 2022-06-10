cf004d8d24a0fda3f3545d08780e36138674f2c4
Andrea Colleoni <andrea.colleoni@outlook.it>
2022-06-10 16:30:22 +0200

    esempio forms in flask

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import request, render_template
+
+import random
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +12,27 @@ def index():
 def saluto():
     studente = { 'nome': 'Andrea', 'cognome': 'Colleoni' }
     return render_template('saluto.html', studente=studente)
+
+
+@app.route('/casuali')
+def casuali():
+    list = []
+    for i in range(0, 9):
+        list.append(random.randint(1, 100))
+    print(list)
+    return render_template('casuali.html', numeri=list)
+
+@app.route('/tabellina', methods=['GET', 'POST'])
+def tabellina():
+    if request.method == 'POST':
+        numero = request.form.get('numero')
+        ripetizioni = request.form.get('ripetizioni')
+
+        print(numero, ripetizioni)
+        list = []
+        for i in range(1, int(ripetizioni) + 1):
+            list.append(i * int(numero))
+        return render_template('tabellina.html', numeri=list, numero=numero)
+
+    elif request.method == 'GET':
+        return render_template('tabellina.html')
