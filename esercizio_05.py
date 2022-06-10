6b4d849fb7ed3f17e467cb77cf67eb5caabbee88
Andrea Colleoni <andrea.colleoni@outlook.it>
2022-06-10 16:37:57 +0200

    esercizio

---
+++ esercizio_05.py
@@ -0,0 +1,40 @@
+# Esercizio su forms con Flask
+
+# Rifare l'esercizio 1 con una user interface web
+
+'''
+Richiedere all'utente (tramite un form) 
+- un numero n
+- un tipo di disegno, selezionabile con un input HTML di tipo SELECT (visualizza una drop down list) e può valere a, b, c o d
+- un simbolo (un singolo carattere)
+
+disegnare, utilizzando il carattere selezionato, le seguenti forme in una pagina web:
+
+Esempi con n = 4
+a) un triangolo
+*
+**
+***
+****
+
+b) un triangolo rovesciato
+****
+***
+**
+*
+
+c) un quadrato n x n pieno
+
+****
+****
+****
+****
+
+d) un quadrato n x n vuoto
+
+****
+*  *
+*  *
+****
+
+'''
